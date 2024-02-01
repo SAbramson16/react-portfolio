989a2a1a0323122d4fd8ea54d7e9f064ae6d8ece
Sharon Abramson <sharabramson@gmail.com>
2024-02-01 09:52:39 -0500

    added pdf button, created resume component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import Header from "./components/Header";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+// import Resume from "./components/Resume";
 import { Outlet } from 'react-router-dom';
 
+
 function App() {
+  
     return (
         <>
           <Header />
           <main className="mx-3">
             <Outlet />
+            {/* <Resume /> */}
           </main>
           <Footer />
         </>
       );
-}
+};
+
 
 export default App;
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,19 @@
-export default function Portfolio() {
-    return (
-      <div>
-        <h1>Resume</h1>
-        <p>
-         Link to resume
-        </p>
-      </div>
-    );
-  }
-  
+import Resume from '../components/Resume'
+
+export default function ResumePage() {
+  const onButtonClick = () => {
+    const pdfUrl = "Sample.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "/src/assets/resume.pdf"; // specify the filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div>
+        <Resume onButtonClick={onButtonClick} />
+    </div>
+  );
+}
